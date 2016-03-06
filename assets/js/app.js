2fe39cabc75eff21109f8c802dc80fe0af80e7f9
Alex Sindalovsky <design@acedzn.com>
2016-03-07 00:14:38 +0200

    - added directives for cleaner look :) - refractor of app.js to work with the directives - added npm package file to install http-server

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,17 @@
 "use strict";
 (function(){
-  angular.module('comments', []).controller('commentWidgetCtrl', function ($scope,Gravatar) {
+  angular.module('comments', [])
+  .controller('newCommentCtrl', function ($scope,Gravatar) {
+    $scope.newComment = {};
+    $scope.addNewComment = function () {
+        $scope.newComment.id = $scope.comments.length + 1;
+        $scope.newComment.content = $scope.newComment.content;
+        $scope.comments.push($scope.newComment);
+        $scope.commentForm.$setPristine();
+        return $scope.newComment = {};
+    };
+  })
+  .controller('commentsListCtrl', function ($scope,Gravatar) {
     $scope.comments = [
       {
         id: 1,
@@ -23,16 +34,46 @@
         content: "Goodbye :)"
       }
     ];
-    $scope.newComment = {};
+  })
+  .controller('singleCommentCtrl', function ($scope,Gravatar) {
     $scope.getAvatar = function(email){
       return Gravatar(email);
     };
-    $scope.addNewComment = function () {
-        $scope.newComment.id = $scope.comments.length + 1;
-        $scope.newComment.content = $scope.newComment.content;
-        $scope.comments.push($scope.newComment);
-        $scope.commentForm.$setPristine();
-        return $scope.newComment = {};
+  })
+  .directive('newCommentForm', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './assets/views/new-comment-form.html',
+      controller: 'newCommentCtrl'
     };
-  });
+  })
+  .directive('commentsList', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './assets/views/comments-list.html',
+      controller: 'commentsListCtrl'
+    };
+  })
+  .directive('filterComments', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './assets/views/filter-comments.html'
+    };
+  })
+  .directive('filterNoComments', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './assets/views/filter-no-comments.html'
+    };
+  })
+  .directive('singleComment', function(){
+    return {
+      restrict: 'E',
+      templateUrl: './assets/views/single-comment.html',
+      controller: 'singleCommentCtrl'
+    };
+  })
+
+
+  ;
 }());
