15efb200bf0d92373f82592faa7946a880e48cc0
Alex Sindalovsky <design@acedzn.com>
2016-03-06 23:21:08 +0200

    -added default comments array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,25 @@
 "use strict";
 (function(){
   angular.module('comments', []).controller('commentWidgetCtrl', function ($scope) {
-    $scope.comments = [];
+    $scope.comments = [
+      {
+        id: 1,
+        author: "elik@bigpanda.io",
+        content: "Hello there. How are you?"
+      },
+      {
+        id: 2,
+        author: "Shai@bigpanda.io",
+        content: "Good!!!"
+      },
+      {
+        id: 3,
+        author: "noam@bigpanda.io",
+        content: "Goodbye :)"
+      }
+    ];
     $scope.newComment = {};
-    
+
     $scope.addNewComment = function () {
         $scope.newComment.id = $scope.comments.length + 1;
         $scope.newComment.content = $scope.newComment.content;
