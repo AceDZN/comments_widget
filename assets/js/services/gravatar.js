dd2a2a1210269ede2955c55e1d550696b13c64ca
Alex Sindalovsky <design@acedzn.com>
2016-03-08 00:53:23 +0200

    * replaced graver default size - for better resolution

--- assets/js/services/gravatar.js
+++ assets/js/services/gravatar.js
@@ -1,5 +1,5 @@
 angular.module('comments').factory('Gravatar', function GravatarFactory(){
-  var avatarSize = 50;
+  var avatarSize = 250;
   var gravatarUrl = 'http://gravatar.com/avatar/';
   return  function(email){
       var hash;
