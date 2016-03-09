b92124f7a07d372426cbe22336d1729b4b2fd80d
Alex Sindalovsky <design@acedzn.com>
2016-03-09 12:07:18 +0200

    :\

--- assets/js/controllers/comments-list-ctrl.js
+++ assets/js/controllers/comments-list-ctrl.js
@@ -1,7 +1,7 @@
 angular.module('comments')
 .controller('commentsListCtrl', function ($scope) {
   $scope.comments = [
-    {id: 1,author: "design@acedzn.com",content: "Hola Amigoz, this is my Comment widget for the Big Panda Exercise"},
+    {id: 1,author: "design@acedzn.com",content: "Hola Amigoz, this is my AngularJS Comment widget"},
     {id: 2,author: "alex@acedzn.com",content: "Hello there. How are you?"},
     {id: 3,author: "another@brother.io",content: "Good!!!"},
     {id: 4,author: "design@acedzn.com",content: "Goodbye :)"}
